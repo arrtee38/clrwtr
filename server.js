49770c843083aee604c3b58ead39c10d529dcad5
arrtee38 <37919458+arrtee38@users.noreply.github.com>
2022-08-20 23:38:16 -0500

    add mongodb connect method

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+
 const app = express();
+
 var corsOptions = {
   origin: "http://localhost:8081"
 };
+
 app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
@@ -12,6 +15,21 @@ app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// database info
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 // test route
 app.get("/", (req, res) => {
